4991f01a179904ec0c627eded0b49dc64e244c34
Marci <yu.gi.ohlover1984@gmail.com>
2022-10-12 18:20:57 -0500

    :construction: progress - emoji comparison

---
+++ main.py
@@ -0,0 +1,29 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import json
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+    async def on_raw_reaction_add(self, payload):
+        print(f'Message reacted: {payload.message_id}, emoji: {payload.emoji}.')
+        print(f'\'{archiveEmote}\'vs\'{payload.emoji}\'')
+        print(archiveEmote.__eq__(payload.emoji))
+        print(archiveEmote == payload.emoji)
+        if (payload.emoji == config["archiveEmote"]):
+            print("Archival Candidate")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+config = json.load(open("configFile"))
+archiveEmote = config["archiveEmote"]
+
+client = MyClient(intents=intents)
+client.run(config["token"])
